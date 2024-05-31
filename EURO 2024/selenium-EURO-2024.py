2f936ca338209af0bc29273842a767e040fcf335
JuanjoRestrepo <restrepojuanjo@gmail.com>
2024-05-30 21:01:53 -0500

    EURO Historical Data 2024 Gotten

--- EURO 2024/selenium-EURO-2024.py
+++ EURO 2024/selenium-EURO-2024.py
@@ -1,12 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=options)
 
 def get_matches_1959():
     print('\nGetting the Matches of Copa America 1959')
@@ -70,43 +64,37 @@ def get_matches(year):
     else:
         web = f'https://en.wikipedia.org/wiki/UEFA_Euro_{year}'
 
-    driver.get(web)
-    matches = driver.find_elements(by='xpath', value='//td[@align="right"]/.. | //td[@style="text-align:right;"]/..')
+    response = requests.get(web)
+    content = response.text #contenido html de la pagina
+    soup = BeautifulSoup(content, 'lxml')
+
+    matches = soup.find_all('div', class_='footballbox')
 
     home = []
     score = []
     away = []
 
     for game in matches:
-        home.append(game.find_element(by='xpath', value='./td[2]').text)
-        score.append(game.find_element(by='xpath', value='./td[3]').text)
-        away.append(game.find_element(by='xpath', value='./td[4]').text)
+        home.append(game.find('th', class_='fhome').get_text())
+        score.append(game.find('th', class_='fscore').get_text())
+        away.append(game.find('th', class_='faway').get_text())
 
-    dict_football = {'home': home, 'score': score, 'away': away}
+    dict_football = {'home': home,
+                    'score': score,
+                    'away': away}
+
     df_football = pd.DataFrame(dict_football)
     df_football['year'] = year
-
     return df_football
 
 # Extract data for all Copa America years
 years = [1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 
-         1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024]
+         1996, 2000, 2004, 2008, 2012, 2016, 2020]
 
-CopaAmerica = []
+Euro = [get_matches(year) for year in years]
 
-# Iterate through the years and call the appropriate function
-for year in years:
-    if year == 1959:
-        df_1959 = get_matches_1959()
-        print(df_1959.head())  # Debugging: print the first few rows of the 1959 dataframe
-        CopaAmerica.append(df_1959)
-    else:
-        df_year = get_matches(year)
-        CopaAmerica.append(df_year)
-
-driver.quit()
 
 # Concatenate all DataFrames into one final DataFrame
-df_CopaAmerica = pd.concat(CopaAmerica, ignore_index=True)
-print(df_CopaAmerica[df_CopaAmerica['year'] == 1959])  # Debugging: print the rows for the year 1959
-df_CopaAmerica.to_csv('EURO_Historical_Data.csv', index=False)
+df_Euro = pd.concat(Euro, ignore_index=True)
+print(df_Euro[df_Euro['year'] == 2012])  # Debugging: print the rows for the year 1959
+df_Euro.to_csv('EURO_Historical_Data.csv', index=False)
